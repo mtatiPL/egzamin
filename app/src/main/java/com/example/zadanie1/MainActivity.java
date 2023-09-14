3de9708b02b20664bc949f3a3915d7fc637df796
mtatiPL <mateusz-biernat@tlen.pl>
2023-09-14 08:49:55 +0200

    koniec

--- app/src/main/java/com/example/zadanie1/MainActivity.java
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.zadanie1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int  polubienia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Button like=findViewById(R.id.button);
+        Button usun=findViewById(R.id.button2);
+        TextView liky=findViewById(R.id.liky);
+        polubienia=0;
+
+
+
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                polubienia++;
+
+                String text=String.valueOf(polubienia);;
+                liky.setText(text+ " polubienia");
+
+            }
+        });
+        usun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (polubienia>0)
+                polubienia--;
+                else
+                    polubienia=0;
+
+                String text=String.valueOf(polubienia);;
+                liky.setText(text+ " polubienia");
+            }
+        });
+
+
     }
+
+
 }
